50f4d24f9c4af2cabc3a33b4f5a7f1c963ca3cf6
Pablo <example@mail.ru>
2021-11-17 18:14:01 +0300

    add new questions

--- js/script.js
+++ js/script.js
@@ -1,19 +1,51 @@
 "use strict";
 
 
-const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '');
 
-const personalMovieDB = {
-count: numberOfFilms,
-movies:{},
-actors:{},
-genres:[],
-privat: false
-};
 
-const a = prompt('Один из последних фильмов?', ''),
-      b = prompt('оценка', '');
-      
-personalMovieDB.movies[a] = b;
+let numberOfFilms;
 
+
+function start(){
+  numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '');
+
+  while (numberOfFilms =='' || numberOfFilms== null || isNaN(numberOfFilms) ){
+    numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '');
+  };
+}
+start();
+  const personalMovieDB = {
+  count: numberOfFilms,
+  movies:{},
+  actors:{},
+  genres:[],
+  privat: false
+  };
+
+
+
+function writeYourGenres(){
+  for( let i=1; i<4; i++){
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+}
+
+}
+writeYourGenres();
 console.log(personalMovieDB);
+
+
+   function ShowMyDB(hidden){
+   if(!hidden){
+     console.log(personalMovieDB.privat);
+   }
+  }
+  ShowMyDB(personalMovieDB.privat);
+
+//  const a = prompt('Один из последних фильмов?', ''),
+//        b = prompt('оценка', '');
+      
+
+
+//  personalMovieDB.movies[a] = b;
+
+//  console.log(personalMovieDB);
